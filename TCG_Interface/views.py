1fa85ef3345dd3c2cbc17bf2d0539b493f86af40
AndyGooShooter <91827827+AndyGooShooter@users.noreply.github.com>
2022-12-15 07:27:03 +0200

    V0 .

--- TCG_Interface/views.py
+++ TCG_Interface/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+logged_in = False
 
-# Create your views here.
+
+
+
+
+'''HOME PAGE'''
+# Parent function: display
+# Permissions:
+# Url: /
+
+def home(request):
+    return(render, 'home.html')
+
+
+
+
+
+'''ACCOUNT COLLECTION DISPLAY'''
+# Parent function: display
+# Permissions: logged_in
+# Url: /
+
+def collection(request):
+    if logged_in == True:
+        collection___logged_in(request)
+    else:
+        home(request)
+
+def collection___logged_in(request):
+    return(render, 'collection.html')
+
+
+
+
+
+#
